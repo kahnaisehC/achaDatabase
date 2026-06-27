928553376de9b6f71cd1c5e0d2d837b84575be2e
Ian Hakanson <gabianhak@gmail.com>
2026-06-27 01:42:18 -0300

    feat: add moves endpoint

--- graph/opening_explorer/backend/main.go
+++ graph/opening_explorer/backend/main.go
@@ -30,8 +30,8 @@ func (cfg *config) handlerGetGames(w http.ResponseWriter, r *http.Request) {
 	if pfen == "" {
 		pfen = initialPFEN
 	}
-	query := `
-	MATCH (p:Position{PFEN: $pfen})-[:Occurred]->(g:Game) return g;`
+	query := `MATCH (p:Position{PFEN: $pfen})-[:Occurred]->(g:Game) return g;`
+
 	result, err := neo4j.ExecuteQuery(context.Background(), cfg.neo4jDB,
 		query,
 		map[string]any{
@@ -68,7 +68,63 @@ func (cfg *config) handlerGetGames(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Write(gameData)
 }
+
+type Move struct {
+	SAN         string
+	NextPFEN    string
+	AmountWhite int
+	AmountBlack int
+	AmountDraw  int
+}
+
 func (cfg *config) handlerGetMoves(w http.ResponseWriter, r *http.Request) {
+	pfen := r.URL.Query().Get("pfen")
+	if pfen == "" {
+		pfen = initialPFEN
+	}
+
+	query := `
+	MATCH (p:Position{PFEN: $pfen})
+	-[m:Move]->
+	(p2:Position) 
+	RETURN
+	m.SAN AS SAN,
+	p2.PFEN AS PFEN, 
+	COUNT{(p2)-[:Occurred]->(g:Game) WHERE g.Result = "1/2-1/2"} AS AmountDraw, 
+	COUNT{(p2)-[:Occurred]->(g:Game) WHERE g.Result = "0-1"} AS AmountBlack, 
+	COUNT{(p2)-[:Occurred]->(g:Game) WHERE g.Result = "1-0"} AS AmountWhite;`
+
+	result, err := neo4j.ExecuteQuery(context.Background(), cfg.neo4jDB,
+		query,
+		map[string]any{
+			"pfen": pfen,
+		},
+		neo4j.EagerResultTransformer)
+	if err != nil {
+		panic(err)
+	}
+
+	// Loop through results and do something with them
+
+	moves := make([]Move, 0, len(result.Records))
+
+	for _, record := range result.Records {
+		move := Move{
+			SAN:         record.AsMap()["SAN"].(string),
+			NextPFEN:    record.AsMap()["NextFEN"].(string),
+			AmountWhite: record.AsMap()["AmountWhite"].(int),
+			AmountBlack: record.AsMap()["AmountBlack"].(int),
+			AmountDraw:  record.AsMap()["AmountDraw"].(int),
+		}
+		moves = append(moves, move)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	gameData, err := json.Marshal(moves)
+	if err != nil {
+		log.Fatal("couldnt marshal moves")
+	}
+	w.Write(gameData)
 
 }
 
@@ -82,7 +138,7 @@ func main() {
 	neo4jUri := os.Getenv("NEO4J_URI")
 	neo4jPass := os.Getenv("NEO4J_PASS")
 
-	query := `MATCH (p:Position{PFEN: "r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq -"})-[:Occurred]->(g:Game) return g.White, g.Black;`
+	query := `MATCH (p:Position{PFEN: "r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R b KQkq -"})-[m:Move]->(p2:Position) return m;`
 
 	fmt.Println("trying to connect to", neo4jUri, " ", neo4jUsr)
 	driver, err := neo4j.NewDriver(neo4jUri, neo4j.BasicAuth(neo4jUsr, neo4jPass, ""))
